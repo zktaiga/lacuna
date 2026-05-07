f723f5cea9a8bdf563dc12759a8ba31c7caa142f
Taiga <taiga@null.net>
2026-05-07 07:09:28 +0400

    fix: redact telegram request logs

--- config/config.exs
+++ config/config.exs
@@ -7,4 +7,6 @@ config :logger, :console,
 config :ex_gram,
   json_engine: Jason
 
+config :ex_gram, Tesla.Middleware.Logger, format: "$method Telegram API -> $status ($time ms)"
+
 import_config "#{config_env()}.exs"
